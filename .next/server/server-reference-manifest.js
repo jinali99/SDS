b85e10000c4a13b3ac2f83250cff88d65c4ca318
Jinali sanghvi <jinali.sanghvi@krishtechnolabs.com>
2024-12-20 16:33:51 +0530

    -Project setup

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NWTpTBd4heGJlroRWHAhIrD4k6B+yf/lBHsH2E7nTqs=\"\n}"
---
+++ .next/static/chunks/src_app_page_7ada19.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_7ada19.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_7ada19.js",
+  "chunks": [
+    "static/chunks/_fbfbb0._.js"
+  ],
+  "source": "dynamic"
+});
